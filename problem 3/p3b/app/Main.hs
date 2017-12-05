1206463557b5e7fa61f5896520939714efc17406
Aiden Jeffrey <aiden.jeffrey@codethink.co.uk>
2017-12-05 13:54:44 +0000

    Finish 3b

--- problem 3/p3b/app/Main.hs
+++ problem 3/p3b/app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Lib
 import Data.List
+import Data.Maybe
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 
@@ -9,6 +10,9 @@ data Coord = Coord {
     x :: Int,
     y :: Int
 } deriving (Show, Eq, Ord)
+
+add_coord :: Coord -> Coord -> Coord
+add_coord a b = (Coord ((x a)+(x b)) ((y a)+(y b)))
 
 data State = State {
     xy :: Coord,
@@ -34,18 +38,48 @@ spiral n
     | n == 1 = State (Coord 0 0) 'r'
     | otherwise = spiral_next $ spiral (n-1)
 
-updateMap :: Map Coord Int -> Int -> Map Coord Int
-updateMap myMap n =
-    Map.insert (Coord 1 0) n myMap
+update_map :: Map Coord Int -> Coord -> Map Coord Int
+update_map my_map xy
+    | xy == (Coord 0 0) = Map.insert xy 1 my_map
+    | otherwise = Map.insert xy val my_map
+    where val = get_neighbour_sum my_map xy
 
+list_to_coord :: [Int] -> Coord
+list_to_coord xy = (Coord (xy !! 0) (xy !! 1))
 
-get_manhattan :: Coord -> Int
-get_manhattan (Coord x y) =
-    (abs x) + (abs y)
+get_neighbour_sum :: Map Coord Int -> Coord -> Int
+get_neighbour_sum my_map xy = do
+    let deltas = map list_to_coord $ filter (\x -> x /= [0,0]) $ sequence [[1,0,-1],[1,0,-1]]
+    let neighbours = map (add_coord xy) deltas
+    let adj_scores = map (\x -> Map.lookup x my_map) neighbours
+    foldl (+) 0 $ catMaybes adj_scores
+
+-- get_manhattan :: Coord -> Int
+-- get_manhattan (Coord x y) =
+--     (abs x) + (abs y)
+
+get_max_score :: Map Coord Int -> Int
+get_max_score my_map
+    | my_map == Map.empty = 0
+    | otherwise = maximum $ Map.elems my_map
+
+-- fold_me :: (Map Coord Int -> Coord -> Map Coord Int) -> Map Coord Int -> [Coord] -> 
+
+takeWhileOneMore :: (a -> Bool) -> [a] -> [a]
+takeWhileOneMore p = foldr (\x ys -> if p x then x:ys else [x]) []
 
 main :: IO ()
 main = do
-    let m = (Map.empty)
-    let k = updateMap m 1
-    print $ map (xy . spiral) [1..200]
-    -- print k
+    let score_map = (Map.empty)
+    let coords_inf = map (xy . spiral) [1..]
+    -- let coords = map (xy . spiral) [1..100]
+
+    let max_allowed = 265149
+
+    -- let map_out = foldl update_map score_map coords
+
+    let map_out = takeWhileOneMore (\m -> (get_max_score m) < max_allowed) $ scanl update_map score_map coords_inf
+
+    print map_out
+    print $ get_max_score $ last map_out
+
